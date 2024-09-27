cd02372167462003fa9007089c55a9f417637bfe
Connie Gildemeister <122182092+ConnieGildemeister@users.noreply.github.com>
2024-09-27 12:06:29 -0700

    Delete Lab3 directory

--- Lab3/modules/utils.js
+++
@@ -1,8 +0,0 @@
-// Note: ChatGPT helped in the making of this code
-
-function getDate(name) {
-    const date = new Date();
-    return `Hello ${name}, What a beautiful day. Server current date and time is ${date}`;
-}
-
-module.exports = { getDate };
