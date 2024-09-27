1595788c68e5dc488bbee4fbf00f995b046b4f5c
Connie Gildemeister <122182092+ConnieGildemeister@users.noreply.github.com>
2024-09-27 12:18:11 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { getDate } = require('./modules/utils');
 
 const server = http.createServer((req, res) => {
     const queryObject = url.parse(req.url, true).query;
-    const name = queryObject.name || 'Connie';
+    const name = queryObject.name || 'Guest';
 
     if (req.url.startsWith('/getDate')) {
         const message = getDate(name);
@@ -21,3 +21,35 @@ const server = http.createServer((req, res) => {
 server.listen(8080, () => {
     console.log('Server running at http://localhost:8080/');
 });
+
+const fs = require('fs');
+const path = require('path');
+
+if (req.url.startsWith('/writeFile')) {
+    const text = queryObject.text || 'No Text';
+    const filePath = path.join(__dirname, 'file.txt');
+
+    fs.appendFile(filePath, text + '\n', (err) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('Error writing to file');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Text appended to file');
+        }
+    });
+}
+
+if (req.url.startsWith('/readFile')) {
+    const filePath = path.join(__dirname, 'file.txt');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end(`File not found: ${filePath}`);
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end(data);
+        }
+    });
+}
